7e213eb5ae9748a61e3cddc3ef8150f60660f14c
oleg <oleg.gusev@gmail.com>
2023-03-13 18:26:45 +0300

    fixed error_message

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from parse_source import parse_source
 
 session = None
 amount = 30
+
+
+
 if __name__ == '__main__':
     print("get_keys")
     i = 0
@@ -23,30 +26,9 @@ if __name__ == '__main__':
     x.start()
 
     while True:
-        print("start")
-        try:
-            # p = multiprocessing.Process(target=parse_key, name="parse_key", args=(None, ))
-            # p.start()
-            # n = 0
-            # while n < 360:
-            #     if p.is_alive():
-            #         n += 1
-            #     else:
-            #         n = 360
-            #     try:
-            #         p.terminate()
-            #     except Exception:
-            #         pass
-            #     try:
-            #         p.join()
-            #     except Exception:
-            #         pass
-            session = parse_key(session)
-        except Exception as e:
-            print(f"parse_key: {e}")
+        # print("start")
         # try:
-        #     session = parse_source(session)
-        #     # p = multiprocessing.Process(target=parse_source, name="parse_source", args=(None,))
+        #     # p = multiprocessing.Process(target=parse_key, name="parse_key", args=(None, ))
         #     # p.start()
         #     # n = 0
         #     # while n < 360:
@@ -62,6 +44,27 @@ if __name__ == '__main__':
         #     #         p.join()
         #     #     except Exception:
         #     #         pass
+        #     session = parse_key(session)
         # except Exception as e:
-        #     print(f"parse_source: {e}")
+        #     print(f"parse_key: {e}")
+        try:
+            session = parse_source(session)
+            # p = multiprocessing.Process(target=parse_source, name="parse_source", args=(None,))
+            # p.start()
+            # n = 0
+            # while n < 360:
+            #     if p.is_alive():
+            #         n += 1
+            #     else:
+            #         n = 360
+            #     try:
+            #         p.terminate()
+            #     except Exception:
+            #         pass
+            #     try:
+            #         p.join()
+            #     except Exception:
+            #         pass
+        except Exception as e:
+            print(f"parse_source: {e}")
         time.sleep(60)
