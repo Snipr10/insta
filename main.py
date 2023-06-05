d20e65654cab9e81a27791e1554ac3c7235c6eb7
oleg.gusev <oleg.gusev@gmail.com>
2023-06-05 14:51:33 +0300

    time.sleep -> 5

--- main.py
+++ main.py
@@ -115,7 +115,9 @@ if __name__ == '__main__':
         except Exception as e:
             print(f"parse_key: {e}")
         try:
-            session = parse_source(session)
+            # session = parse_source(session)
+            session = parse_key(session)
+
             # p = multiprocessing.Process(target=parse_source, name="parse_source", args=(None,))
             # p.start()
             # n = 0
