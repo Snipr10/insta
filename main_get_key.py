32f08f848e13d01e050473cf4baa37e60f8060d1
oleg.gusev <oleg.gusev@gmail.com>
2023-10-03 12:28:10 +0300

    2.0.0

--- main_get_key.py
+++ main_get_key.py
@@ -20,7 +20,7 @@ def get_keys():
     def callback(ch, method, properties, body):
         try:
             print(f"KEYS {len(KEYS)}")
-            while len(KEYS) > 1000:
+            while len(KEYS) > 10:
                 time.sleep(2)
             KEYS.append(json.loads(body.decode("utf-8")))
         except Exception as e:
