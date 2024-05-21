88f9ad15b96637a4e79533afbe6834c129a956c5
oleg.gusev <gusevoleg@gmail.com>
2024-05-21 23:09:35 +0300

    2.1.2

--- main_get_key.py
+++ main_get_key.py
@@ -19,10 +19,14 @@ def get_keys():
 
     def callback(ch, method, properties, body):
         try:
+            from utils import SESSIONS
+
             print(f"KEYS {len(KEYS)}")
             while len(KEYS) > 3:
                 time.sleep(2)
                 print(f"KEYS {len(KEYS)}")
+                print(f"SESSIONS {len(SESSIONS)}")
+
             KEYS.append(json.loads(body.decode("utf-8")))
         except Exception as e:
             print(f"callback{e}")
